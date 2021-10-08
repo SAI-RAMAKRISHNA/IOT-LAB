5f5505cba182e01660e78df4d9ef4482d8f3f529
SAI-RAMAKRISHNA <71307060+SAI-RAMAKRISHNA@users.noreply.github.com>
2021-10-08 15:50:58 +0530

    Add files via upload

---
+++ sketch_oct08a/sketch_oct08a.ino
@@ -0,0 +1,25 @@
+int pin_red=13;
+int pin_green=2;
+int pin_yellow=5;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(pin_red,OUTPUT);
+ pinMode(pin_green,OUTPUT);
+ pinMode(pin_yellow,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pin_red, HIGH);
+delay(1000);
+digitalWrite(pin_red, LOW);
+delay(1000);
+digitalWrite(pin_yellow, HIGH);
+delay(1000);
+digitalWrite(pin_yellow, LOW);
+delay(1000);
+digitalWrite(pin_green, HIGH);
+delay(1000);
+digitalWrite(pin_green, LOW);
+delay(1000);
+}
---
+++ sketch_oct0GSRK/sketch_oct0GSRK.ino
@@ -0,0 +1,25 @@
+int pin_red=2;
+int pin_green=5;
+int pin_yellow=13;
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(pin_red,OUTPUT);
+ pinMode(pin_green,OUTPUT);
+ pinMode(pin_yellow,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pin_red, HIGH);
+digitalWrite(pin_green, LOW);
+digitalWrite(pin_yellow, LOW);
+delay(1000);
+digitalWrite(pin_green, HIGH);
+digitalWrite(pin_red, LOW);
+digitalWrite(pin_yellow, LOW);
+delay(5000);
+digitalWrite(pin_yellow, HIGH);
+digitalWrite(pin_red, LOW);
+digitalWrite(pin_green, LOW);
+delay(10000);
+}
