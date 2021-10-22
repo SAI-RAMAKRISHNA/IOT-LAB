2dc95d59c593dafe0e7243b2177435e1e94e3e52
SAI-RAMAKRISHNA <71307060+SAI-RAMAKRISHNA@users.noreply.github.com>
2021-10-22 15:39:06 +0530

    Add files via upload

---
+++ gsrk1/gsrk1.ino
@@ -0,0 +1,24 @@
+const int buttonpin =3;
+const int ledpin = 13;
+
+int buttonstate = 0;
+void setup() {
+  pinMode(ledpin,OUTPUT);
+  pinMode(buttonpin,INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  buttonstate=digitalRead(buttonpin);
+
+  if(buttonstate==HIGH){
+
+    digitalWrite(ledpin,HIGH);
+  }else{
+    digitalWrite(ledpin,LOW);
+  }
+}
+
+  // put your main code here, to run repeatedly:
+
---
+++ gsrk2/gsrk2.ino
@@ -0,0 +1,20 @@
+int ledpin=11;
+void setup() {
+  pinMode(ledpin,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int i;
+  for(i=0;i<256;i++);{
+  // put your main code here, to run repeatedly:
+ analogWrite(ledpin,i);
+ delay(2);
+}
+for(i>=255;i>=0;i--){
+  analogWrite(ledpin,i);
+  delay(2);
+}
+}
+
